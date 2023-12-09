af7f4badb3970364e30af9f3e753b4cf0b71ebeb
Illumanizer <74318570+Illumanizer@users.noreply.github.com>
2023-12-10 03:07:23 +0530

    add problem

--- Coin_Combinations_I.cpp
+++ Coin_Combinations_I.cpp
@@ -1,8 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+const int mod=1e9+7;
 
 int main()
 {
+    ll n,x;
+    cin>>n>>x;
+
+    vector<ll> coins(n);
+    for(ll i=0;i<n;i++)
+        cin>>coins[i];
+
+    vector<ll> dp(x+1,0);
+    dp[0]=1;
+
+    for(ll i=0;i<=x;i++)
+    {
+        for(ll j=0;j<n;j++)
+        {
+            if(i-coins[j]>=0)
+                dp[i]=(dp[i-coins[j]]+dp[i])%mod;
+        }
+    }
+    cout<<dp[x]<<endl;
     return 0;
 }
